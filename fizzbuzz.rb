5db7826fb1f42d35abf150fa1a8b2485b0d94f1f
serendipidoussophia <scurranmoore@gmail.com>
2018-07-24 18:40:44 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,8 @@
 def fizzbuzz_game(number)
-  if #{number} % 5 == 0 && % 3 == 0
-    print "Buzz"
-    elsif #{number} % 3 == 0 && % 5 > 0
+  if number % 5 == 0 && % 3 == 0
+    print "Fizzbuzz"
+    elsif number % 3 == 0 && % 5 > 0
+    print "F"
   end
 end
 
